5927b81676b7d1aa975df2c8ca97c6492f3e80a8
github Sarthak4730 <sarthakasm07@gmail.com>
2025-06-24 12:50:33 +0530

    Step-1-Setting up Frontend (PORT=3000) & Step-2-Setting up Backend (PORT=5000) done, next up, Step-3-MongoDB Connection

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use( cors() );
+app.use( express.json() );
+
+app.get( '/', (req, res) => res.send("server.js Running at '/' endpoint") );
+
+const PORT = process.env.PORT || 69;
+app.listen( PORT, () => console.log(`Server running on PORT=${PORT}`) );
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Recipe MERN Project Homepage (main.jsx -&gt; app.jsx)</h1>
+    </>
+  )
+}
+
+export default App
